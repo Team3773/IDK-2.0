59d2170c558f8384258a43da115a366922666973
Arrowhead <arrowheadrobotics3773@gmail.com>
2021-03-01 22:29:18 -0500

    first commit

--- src/main/java/frc/robot/subsystems/ballOutakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/ballOutakeSubsystem.java
@@ -13,23 +13,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.canConstant;
 
 public class ballOutakeSubsystem extends SubsystemBase {
-<<<<<<< HEAD
-  private final WPI_VictorSPX ballOutake1 = new WPI_VictorSPX(canConstant.ballOutakePort1);
-  private final WPI_TalonSRX ballOutake = new WPI_TalonSRX(canConstant.ballOutakePort);
-=======
   private final WPI_VictorSPX ballOutake = new WPI_VictorSPX(canConstant.ballOutakePort);
   private final WPI_VictorSPX ballOutake1 = new WPI_VictorSPX(canConstant.ballOutakePort1);
->>>>>>> fd0a7b596f1d51955a38719051db898bb7f30577
 
 
   public ballOutakeSubsystem() {}
 
   public void setmotor(double speed) {
-<<<<<<< HEAD
-    ballOutake.set(-speed);
-=======
     ballOutake.set(speed);
->>>>>>> fd0a7b596f1d51955a38719051db898bb7f30577
     ballOutake1.set(speed);
   }
 
